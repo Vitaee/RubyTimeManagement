a8fc93892a42cb35325b0b28697ed76e6e3262aa
Can İlgu <canilguu@gmail.com>
2022-12-02 10:54:34 +0300

    code refactored in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,33 +4,34 @@ class AdminController < ApplicationController
         
         @user_count = User.all.where("id != ?", current_user.id)
         if params[:username] || params[:time_type] || params[:start_date] || params[:end_date]
+            
             param = give_me_filter_params
             @all_users = nil
             q = nil
+            @user = nil
             
+            if param['username']
+                @user = User.lower_username(param['username'].downcase)
+            end
+
             if param['group_by']
                 if param['group_by'] == 'monthly'
-                    q = 'created_at BETWEEN ? AND ?', DateTime.now.beginning_of_month, DateTime.now.end_of_month
+                    q = 'start_date BETWEEN ? AND ?', DateTime.now.beginning_of_month, DateTime.now.end_of_month
                 else
-                    q = 'created_at BETWEEN ? AND ?', DateTime.now.beginning_of_week, DateTime.now.end_of_week
+                    q = 'start_date BETWEEN ? AND ?', DateTime.now.beginning_of_week, DateTime.now.end_of_week
                 end
             end
 
 
             if  param['username'] && param['time_type'] && param['start_date'] && param['end_date']
-                @user = User.lower_username(param['username'].downcase) 
-
-                start_date = Date.strptime(param['start_date'], "%Y-%m-%d")
-                end_date = Date.strptime(param['end_date'],"%Y-%m-%d")
-
-                @all_users = TimeRecord.by_user_type_dates(start_date.beginning_of_day..end_date.end_of_day, 
+                 
+                @all_users = TimeRecord.by_user_type_dates(param['start_date'].beginning_of_day..param['end_date'].end_of_day, 
                 @user, param['time_type'])
                 .paginate(:page => params[:page], :per_page => 5)
 
             elsif param['username'] && param['time_type'] && param['group_by'] && @all_user.blank?
-                @user = User.lower_username(param['username'].downcase) 
 
-                @all_users = TimeRecord.where(q).where(time_type:param['time_type'], user_id:@user)
+                @all_users = TimeRecord.where(q).by_user_type(@user, param['time_type'])
                 .paginate(:page => params[:page], :per_page => 5)
 
             elsif param['time_type'] && param['group_by'] && @all_users.blank?
@@ -39,37 +40,30 @@ class AdminController < ApplicationController
                 .paginate(:page => params[:page], :per_page => 5)
  
             elsif param['username'] && param['group_by'] && @all_users.blank?
-                @user = User.lower_username(param['username'].downcase) 
 
                 @all_users = TimeRecord.where(q).where(user_id:@user)
                 .paginate(:page => params[:page], :per_page => 5)  
 
             elsif param['username'] && param['start_date'] && param['end_date'] && @all_users.blank? 
-                @user = User.lower_username(param['username'].downcase)
                 
-                start_date = Date.strptime(param['start_date'], "%Y-%m-%d")
-                end_date = Date.strptime(param['end_date'],"%Y-%m-%d")
-                
-                @all_users = TimeRecord.where(start_date: start_date.beginning_of_day..end_date.end_of_day, user_id:@user)
+                @all_users = TimeRecord.by_user_dates(@user, 
+                param['start_date'].beginning_of_day..param['end_date'].end_of_day)
                 .paginate(:page => params[:page], :per_page => 5)
             
             elsif param['time_type'] && param['start_date'] && param['end_date'] && @all_users.blank? 
-                start_date = Date.strptime(param['start_date'], "%Y-%m-%d")
-                end_date = Date.strptime(param['end_date'],"%Y-%m-%d")
                 
-                @all_users = TimeRecord.where(start_date: start_date.beginning_of_day..end_date.end_of_day, time_type: param['time_type'])
+                @all_users = TimeRecord.where(start_date: param['start_date'].beginning_of_day..param['end_date'].end_of_day, 
+                time_type: param['time_type'])
                 .paginate(:page => params[:page], :per_page => 5)
 
             elsif param['start_date'] && param['end_date'] && @all_users.blank?
-                start_date = Date.strptime(param['start_date'], "%Y-%m-%d")
-                end_date = Date.strptime(param['end_date'],"%Y-%m-%d")
-                
-                @all_users = TimeRecord.where(start_date: start_date.beginning_of_day..end_date.end_of_day)
+
+                @all_users = TimeRecord.where(start_date: param['start_date'].beginning_of_day..param['end_date'].end_of_day)
                 .paginate(:page => params[:page], :per_page => 5)
 
             elsif param['time_type'] && param['username'] && @all_users.blank?
-                @user = User.lower_username(param['username'].downcase) 
-                @all_users = TimeRecord.where(user_id: @user, time_type: params[:time_type] ).paginate(:page => params[:page], :per_page => 5) 
+                @all_users = TimeRecord.by_user_type(@user, param['time_type'])
+                .paginate(:page => params[:page], :per_page => 5) 
                 
 
             elsif param['time_type'] && @all_user.blank?
@@ -77,7 +71,6 @@ class AdminController < ApplicationController
                 .paginate(:page => params[:page], :per_page => 5)  
             
             elsif param['username'] &&  @all_users.blank?
-                @user = User.lower_username(param['username'].downcase)
                 
                 @all_users = TimeRecord.where(user_id: @user ).order(start_date: :desc)
                 .paginate(:page => params[:page], :per_page => 5) 
@@ -103,6 +96,12 @@ class AdminController < ApplicationController
 
     def give_me_filter_params
         params.delete_if{|key,value| value.blank? || value=='---' || value=="Search"}
+        
+        if params['start_date']
+            params['start_date'] =  Date.strptime(params['start_date'], "%Y-%m-%d")
+            params['end_date'] = Date.strptime(params['end_date'], "%Y-%m-%d")
+        end
+
         return params
     end
 end
--- app/models/time_record.rb
+++ app/models/time_record.rb
@@ -4,4 +4,8 @@ class TimeRecord < ApplicationRecord
     scope :by_user_type_dates, ->(date, user, type) { where(start_date: date, user_id: user,
     time_type:type) }
 
+    scope :by_user_type, ->(user, type) { where(user_id: user, time_type: type) }
+
+    scope :by_user_dates, ->(user, date) { where(user_id: user, start_date: date) }
+
 end
