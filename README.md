1d908867a57826a3d3a323e44cf85b6b32419193
Can İlgu <canilguu@gmail.com>
2022-12-02 11:56:18 +0300

    database seeds created.

--- README.md
+++ README.md
@@ -2,18 +2,31 @@
 This is my first ruby on rails project. I learned a lot about the framework, while developing this. Steps are necessary to get the application up and running.
 
 ### Things you may want to cover:
+* System information
+    - Ubuntu 22.04.1 LTS 64-bit
 
 * Ruby version
     - ruby 3.0.4p208 (2022-04-12 revision 3fa771dded) [x86_64-linux]
     - Rails 7.0.4
 
 * Configuration
+    - create docker postgresql and our database with these commands:
+    ```.bash 
+        sudo docker run --name some-postgres -e POSTGRES_PASSWORD=123456 -d postgres
+        sudo docker exec -it some-postgres psql -U postgres -c "create database time_management_development" 
+    ```
+    - Our database is ready to use. So know, install dependencies with bundle.
     - ```bundle install```
-    - create .env in project root and put your ```DB_HOST``` to .env file
+    - create .env in project root and put your variables to .env file
     ```.env
     DB_HOST=172.17.0.2
+    DB_USER=postgres
+    DB_PASSWORD=123456
+    DB_HOST=172.17.0.2
+    DB_PORT=5432
     ```
-    - rails db:migrate
+    - The last step before ```rails s``` is migrating our tables, please run this command ```rails db:migrate```
+    
 
 ### Example video shows demo about the application.
 [![Watch the video](https://i.ibb.co/x6Wvf1j/denemee.png)](https://streamable.com/e/ud3udl)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+TimeRecord.destroy_all
+
+t_records = TimeRecord.create( [ 
+    { id: 1, start_date: DateTime.now, time_type: "bug", comment: "task have bug" },
+    { id: 2, start_date: DateTime.now, end_date: DateTime.now + 5.hours, time_type: "issue", comment: "task have another issue" },
+    { id: 3, start_date: DateTime.now - 1.days, time_type: "issue", comment: "task have issue" },
+    { id: 4, start_date: DateTime.now- 2.days, end_date: DateTime.now + 1.hours, time_type: "feature", comment: "task have feature" },
+    { id: 5, start_date: DateTime.now - 4.hours, end_date: DateTime.now + 2.hours, time_type: "done", comment: "task #123 done" },
+    { id: 6, start_date: DateTime.now- 11.hours, end_date: DateTime.now + 3.hours, time_type: "done", comment: "task #345 done" },
+    { id: 7, start_date: DateTime.now - 4.hours, end_date: DateTime.now + 4.hours, time_type: "bug", comment: "task #451 have bug" },
+    { id: 8, start_date: DateTime.now - 12.hours, end_date: DateTime.now + 2.hours, time_type: "feature", comment: "task #321 have new feature" },
+
+])
+
+p "Created #{t_records.count} Time Records"
+
+
+users = User.create([{
+    username: "Joe",
+    email: "joe@gmail.com",
+    password: "123456",
+    is_admin: false,
+    time_records: t_records[0..2] 
+},
+{
+    username: "Doe",
+    email: "doe@gmail.com",
+    password: "123456",
+    is_admin: false,
+    time_records: t_records[3..4] 
+},
+{
+    username: "can",
+    email: "can@gmail.com",
+    password: "123456",
+    is_admin: false,
+    time_records: t_records[5..8] 
+},
+{
+
+    username: "admin",
+    email: "admin@gmail.com",
+    password: "123456",
+    is_admin: true,
+}
+])
+
+
+
+p "Created #{User.count} Users"
+p "Login as a admin email: admin@gmail.com , password: 123456"
+p "Login as a user email: can@gmail.com , password: 123456"
